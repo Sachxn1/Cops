685a7658664243561c2dd75755998bef6ae08be1
Sachin Kharabanda <sachin.kharabanda@aol.com>
2024-09-15 15:39:01 +0100

    Initial commit

---
+++ src/main/java/CopsMain.java
@@ -0,0 +1,12 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CopsMain extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
